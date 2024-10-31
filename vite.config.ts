f2e9fd1c6ea86bf6e366cdf1fa943adaf43cc4f6
leesangha <sanghalee1@naver.com>
2024-10-31 14:12:51 +0900

    add: vite 설정 주석

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+// vite library mode로 만들기 위한 라이브러리. *.d.ts .tsx 타입의 파일을 생성해줌
 import dts from "vite-plugin-dts";
 import { globSync } from "glob";
 import path, { resolve } from "node:path";
@@ -15,8 +16,20 @@ export default defineConfig({
   build: {
     lib: { entry: resolve(__dirname, "src/main.ts"), formats: ["es"] },
     rollupOptions: {
+      // 외부 종속성
       external: ["react", "react-dom", "react/jsx-runtime"],
       // https://rollupjs.org/configuration-options/#input
+      // entry point를 지정
+      /** 
+       * {
+          main: '/Users/sanghalee/Documents/infludeo/infludeo-component-test/src/main.ts',
+          'components/MyTitle/index': 
+            '/Users/sanghalee/Documents/infludeo/infludeo-component-test/src/components/MyTitle/index.tsx',
+          'components/MyButton/index': 
+            '/Users/sanghalee/Documents/infludeo/infludeo-component-test/src/components/MyButton/index.tsx'
+          }
+       * 
+       */
       input: Object.fromEntries(
         globSync(["src/components/**/index.tsx", "src/main.ts"]).map(
           (file: any) => {
@@ -31,8 +44,11 @@ export default defineConfig({
         )
       ),
       output: {
+        // main.js , components/MyTitle/index.js , components/MyButton/index.js 형태의 output 생성
         entryFileNames: "[name].js",
+        // https://rollupjs.org/configuration-options/#output-assetfilenames
         assetFileNames: "assets/[name][extname]",
+        // 외부 의존성 명시
         globals: {
           react: "React",
           "react-dom": "React-dom",
