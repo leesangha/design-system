a6d54820a518ebca782403ef3f5e1634f7a991e0
leesangha <sanghalee1@naver.com>
2024-11-01 12:41:48 +0900

    shadcn test

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
+import "./index.css";
 export { MyButton } from "./components/MyButton";
 export { MyTitle } from "./components/MyTitle";
+export { Button as CnButton } from "./components/ui/index";
