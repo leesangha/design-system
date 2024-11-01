685aff70045d9e5d6520f284e6459c85ca8f069e
leesangha <sanghalee1@naver.com>
2024-11-01 14:30:54 +0900

    remove : unnecessary

--- src/components/MyTitle/index.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from "./styles.module.css";
-import clsx from "clsx";
-export function MyTitle({ primary = false, title, ...props }) {
-    const style = clsx(styles.title, {
-        [styles["title--primary"]]: primary,
-    });
-    return (_jsx("h1", { className: style, ...props, children: title }));
-}
