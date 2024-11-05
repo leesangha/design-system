c33f00b378bf9771b6c2cac2d7653828663d9cac
leesangha <sanghalee1@naver.com>
2024-11-05 18:43:46 +0900

    remove: initial Button

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import { Button } from "./Button";
+import { Button } from "@/main";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -15,7 +15,7 @@ const meta = {
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    variant: { control: "text" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -27,27 +27,29 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    variant: "default",
+    size: "lg",
+    children: "Button",
   },
 };
 
-export const Secondary: Story = {
+export const Destructive: Story = {
   args: {
-    label: "Button",
+    variant: "destructive",
+    children: "Button",
   },
 };
 
-export const Large: Story = {
+export const Outline: Story = {
   args: {
-    size: "large",
-    label: "Button",
+    variant: "outline",
+    children: "Button",
   },
 };
 
-export const Small: Story = {
+export const Ghost: Story = {
   args: {
-    size: "small",
-    label: "Button",
+    variant: "ghost",
+    children: "Button",
   },
 };
